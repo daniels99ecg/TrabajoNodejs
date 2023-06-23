c438491e252ca308d1eb62fe8014fb8e450f4960
daniel <emi--lio--@hotmail.com>
2023-06-23 08:43:25 -0500

    Agregando el unwind

---
+++ View/script/login.js
@@ -0,0 +1,16 @@
+function valida(){
+   
+
+        const usuario=document.getElementById('usuario').value;
+        const contra=document.getElementById('contrasena').value;
+        
+        
+        
+        if(usuario=="danielsama" && contra=="123"){
+            console.log('ingreso')
+            
+        }
+
+
+
+}
